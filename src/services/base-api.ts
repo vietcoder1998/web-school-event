2c7f32f20927191736c757441abdf4826119bb07
unknown <32127615+Flamesh@users.noreply.github.com>
2020-05-01 10:19:50 +0700

    1

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -3,45 +3,49 @@ import { authHeaders } from './auth';
 
 // POST
 export const _post = async (data?: any, api?: string, another_host?: string, headers?: any, params?: string) => {
-    let requestURL = (another_host? another_host : process.env.REACT_APP_API_HOST) + api;
+    let requestURL = (another_host ? another_host : process.env.REACT_APP_API_HOST) + api;
     if (headers === null || headers === undefined) {
         headers = authHeaders;
     }
-    let response = await axios.post(requestURL, data, { headers, params });
+    try{
+          let response = await axios.post(requestURL, data, { headers, params });
     return response.data
+    }
+    catch(e){
+        console.log(e)
+    }
+  
 };
 
 //GET
 export const _get = async (params?: any, api?: string, another_host?: string, headers?: any) => {
-    let requestURL = (another_host? another_host : process.env.REACT_APP_API_HOST) + api;
+    let requestURL = (another_host ? another_host : process.env.REACT_APP_API_HOST) + api;
     if (headers === null || headers === undefined) {
         headers = authHeaders
     }
-    try{
-        let response = await axios.get(requestURL, { params: params, headers });
-        return response.data
-    }
-    catch(e) {
-        console.log(e.response.data)
-    }
-    }
 
-  
+    let response = await axios.get(requestURL, { params: params, headers });
+    return response.data
+
+
+}
+
+
 
 // DELETE
 export const _delete = async (data?: any, api?: string, another_host?: string, headers?: any, params?: string) => {
-    let requestURL = (another_host? another_host : process.env.REACT_APP_API_HOST) + api;
+    let requestURL = (another_host ? another_host : process.env.REACT_APP_API_HOST) + api;
     if (headers === null || headers === undefined) {
         headers = authHeaders
     }
-    let response = await axios.delete(requestURL,  { data: params, headers });
+    let response = await axios.delete(requestURL, { data: params, headers });
     return response.data;
 };
 
 
 // PUT
 export const _put = async (data?: any, api?: string, another_host?: string, headers?: any, params?: string) => {
-    let requestURL = (another_host? another_host : process.env.REACT_APP_API_HOST) + api;
+    let requestURL = (another_host ? another_host : process.env.REACT_APP_API_HOST) + api;
     if (headers === null || headers === undefined) {
         headers = authHeaders
     }
