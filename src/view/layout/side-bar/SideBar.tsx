759529f3d7d8ab6f5d56799e6e6de2aeeae80d17
Trần Duy Việt <duyviet2841998@gmail.com>
2020-12-02 10:49:57 +0700

    fix bug

--- src/view/layout/side-bar/SideBar.tsx
+++ src/view/layout/side-bar/SideBar.tsx
@@ -8,6 +8,7 @@ import { REDUX } from '../../../const/actions';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 //@ts-ignore
 import logo from "../../../assets/image/logo-01.png";
+import { Link } from "react-router-dom"
 
 
 class SideBar extends Component {
@@ -39,28 +40,28 @@ class SideBar extends Component {
             <Drawer
                 placement="left"
                 title={
-                    <LazyLoadImage src={logo} alt="logo" width="240" height="80" style={{marginLeft: -20}} />
+                    <LazyLoadImage src={logo} alt="logo" width="240" height="80" style={{ marginLeft: -20 }} />
                 }
                 visible={show}
                 onClose={() => this._closeSideBar()}
             >
-                <div className="content-sidebar">
+                <div className="content-sidebar" onClick={() => this._closeSideBar()}>
                     <ul>
-                        <li><a href={'/'}><Icon type="home" />Trang chủ</a></li>
-                        <li><a href='/event' style={{ display: eventStart === false ? 'none' : window.location.pathname === '/' ? "none" : '' }}><Icon type="tags" />Sự kiện</a></li>
+                        <li><Link to={'/'}><Icon type="home" />Trang chủ</Link></li>
+                        <li><Link to='/event' style={{ display: eventStart === false ? 'none' : window.location.pathname === '/' ? "none" : '' }}><Icon type="tags" />Sự kiện</Link></li>
 
-                        <li style={{ display: isAuthen ? 'block' : 'none' }}><a href='/gross-to-net'><Icon type="dollar" /><label className='text-icon'>Công cụ tính lương</label></a></li>
+                        <li style={{ display: isAuthen ? 'block' : 'none' }}><Link to='/gross-to-net'><Icon type="dollar" /><label className='text-icon'>Công cụ tính lương</label></Link></li>
 
-                        <li style={{ color: 'gray', cursor: 'not-allowed', opacity: 0.5, pointerEvents: 'none' }}><a href='/'><Icon type="user-add" />Tạo CV</a></li>
-                        <li style={{ display: isAuthen ? 'block' : 'none' }}><a href='/result'><Icon type="search" /><label className='text-icon'>Tìm việc</label></a></li>
+                        <li style={{ color: 'gray', cursor: 'not-allowed', opacity: 0.5, pointerEvents: 'none' }}><Link to='/'><Icon type="user-add" />Tạo CV</Link></li>
+                        <li style={{ display: isAuthen ? 'block' : 'none' }}><Link to='/result'><Icon type="search" /><label className='text-icon'>Tìm việc</label></Link></li>
 
-                        <li style={{ display: isAuthen ? 'block' : 'none' }}><a href='/notifications'><Icon type="notification" /><label className='text-icon'>Thông báo</label></a></li>
-                        <li style={{ display: isAuthen ? 'block' : 'none' }}><a href='/profile'><Icon type="user" /><label className='text-icon'>Hồ sơ</label></a></li>
-                        <li style={{ display: isAuthen ? 'block' : 'none' }}><a href='lich-su-ung-tu'><Icon type="solution" /><label className='text-icon'>Lịch sử ứng tuyển</label></a></li>
-                        <li style={{ display: isAuthen ? 'block' : 'none' }}><a href='/cong-viec-da-luu'><Icon type="history" /><label className='text-icon'>Công việc đã lưu</label></a></li>
-                        <li style={{ display: isAuthen ? 'block' : 'none' }} onClick={this._clearStorage}><a style={{ pointerEvents: "none" }} href='/'><Icon type="logout" /><label className='text-icon'>Đăng xuất</label></a></li>
-                        <li style={{ display: isAuthen ? 'none' : 'block' }}><a href='/login'><Icon type="key" /><label className='text-icon'>Đăng nhập</label></a></li>
-                        <li style={{ display: isAuthen ? 'none' : 'block' }}><a href='/register'><Icon type="solution" /><label className='text-icon'>Đăng kí</label></a></li>
+                        <li style={{ display: isAuthen ? 'block' : 'none' }}><Link to='/thong-bao'><Icon type="notification" /><label className='text-icon'>Thông báo</label></Link></li>
+                        <li style={{ display: isAuthen ? 'block' : 'none' }}><Link to='/profile'><Icon type="user" /><label className='text-icon'>Hồ sơ</label></Link></li>
+                        <li style={{ display: isAuthen ? 'block' : 'none' }}><Link to='lich-su-ung-tuyen'><Icon type="solution" /><label className='text-icon'>Lịch sử ứng tuyển</label></Link></li>
+                        <li style={{ display: isAuthen ? 'block' : 'none' }}><Link to='/cong-viec-da-luu'><Icon type="history" /><label className='text-icon'>Công việc đã lưu</label></Link></li>
+                        <li style={{ display: isAuthen ? 'block' : 'none' }} onClick={this._clearStorage}><Link style={{ pointerEvents: "none" }} to='/'><Icon type="logout" /><label className='text-icon'>Đăng xuất</label></Link></li>
+                        <li style={{ display: isAuthen ? 'none' : 'block' }}><Link to='/login'><Icon type="key" /><label className='text-icon'>Đăng nhập</label></Link></li>
+                        <li style={{ display: isAuthen ? 'none' : 'block' }}><Link to='/register'><Icon type="solution" /><label className='text-icon'>Đăng kí</label></Link></li>
                     </ul>
                 </div>
             </Drawer>
